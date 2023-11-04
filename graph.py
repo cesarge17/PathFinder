101e672578332b7cd8a9273ec7c698b9c78e2dc8
cesar <cesar.gismera.elias@alumnos.upm.es>
2023-11-04 20:56:36 +0100

    Added the maze generating algorithm

--- graph.py
+++ graph.py
@@ -1,18 +1,18 @@
 class graph:
     def __init__(self):
         self.graph_dict = {}
-    def add_nodo(self, nodo):
-        self.graph_dict[nodo] = []
-    def add_arista(self, arista):
-        n1 = arista.get_n1()
-        n2 = arista.get_n2()
+    def add_node(self, node):
+        self.graph_dict[node] = []
+    def add_edge(self, edge):
+        n1 = edge.get_n1()
+        n2 = edge.get_n2()
         self.graph_dict[n1].append(n2)
         self.graph_dict[n2].append(n1)
-    def get_aristas(self):
+    def get_edges(self):
         return self.graph_dict
 
 
-class nodo:
+class node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -23,7 +23,7 @@ class nodo:
     def __str__(self):
         return f"({self.x},{self.y})"
     
-class arista:
+class edge:
     def __init__(self, n1, n2):
         self.n1 = n1
         self.n2 = n2
@@ -33,3 +33,22 @@ class arista:
         return self.n2
     def __str__(self):
         return f"({self.n1.__str__()}, {self.n2.__str__()})"
+    
+
+""""
+graph = graph()
+
+
+node1 = node(1,2)
+node2 = node(3,4)
+
+
+graph.add_node(node1)
+graph.add_node(node2)
+
+graph.add_edge(edge(node1, node2))
+
+for node in graph.get_edges().values():
+    for value in node:
+        print(value) 
+"""
--- maze.py
+++ maze.py
@@ -1,15 +1,84 @@
-import graph
+from graph import *
+import random
 
 class maze:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+
+        self.maze_graph = graph()
+
+    def get_maze_graph(self):
+        return self.maze_graph
+
+    def create_maze(self):
         
-        fila = [True] *width
-        self.visitados = [[fila]*height]
-    def get_visitados(self):
-        return self.visitados
+        visited = [[False] * self.width for _ in range(self.height)]
+        stack = []
+        
+        
+        current_node = node(0, 0)
+        stack.append(current_node)
+        self.maze_graph.add_node(current_node) 
+        visited[current_node.get_x()][current_node.get_y()] = True      
+        
+        while True:
+            next_node = self.select_neighbour(current_node.get_x(), current_node.get_y(), visited)
+            
+           
+
+            if next_node!=None:
+                stack.append(next_node)
+                self.maze_graph.add_node(next_node)
+                self.maze_graph.add_edge(edge(current_node, next_node))
+                current_node = next_node
+                visited[current_node.get_x()][current_node.get_y()] = True      
+
+            else:
+                stack.pop()
+                if stack:
+                    current_node = stack[-1]
+
+            for edges, manuel in self.maze_graph.get_edges():
+                print(edges.__str__())
+                
+                
+
+            user_input = input()
+
+            if not stack:
+                break
+
+        
+        
 
 
-maze = maze(5,130)
-print( maze.get_visitados())
+
+    def select_neighbour(self, current_x, current_y, visited):
+        directions  = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        if current_x<=0:
+            directions.remove((-1,0))
+        if current_y<=0:
+            directions.remove((0,-1))
+        if current_x>=self.height-1 :
+            directions.remove((1,0))
+        if current_y>=self.width-1 :
+            directions.remove((0,1))
+
+        for choices in directions.copy():
+            if(visited[current_x + choices[0]][current_y + choices[1]]==True):
+                directions.remove(choices)
+        
+        
+        if(not directions):
+            return None
+        
+        else:
+            direction = random.choice(directions)
+            return node(current_x + direction[0], current_y + direction[1])
+        
+mari = maze(5, 5)
+
+
+mari.create_maze()
